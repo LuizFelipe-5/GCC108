89c47439700cb55bccaf4499aad6ec19a5e1034c
Luiz Felipe Montuani <luiz@zeester.com.br>
2021-10-06 00:50:24 -0300

    separate transations and input(w)

--- argumento1.py
+++ argumento1.py
@@ -7,3 +7,9 @@ except:
     print("Não foi possível abrir o arquivo")
 
 text = file.read().splitlines()
+
+transitions = []
+input = text[-1]
+
+for line in text[1:len(text)-2]:
+    transitions.append(line)
