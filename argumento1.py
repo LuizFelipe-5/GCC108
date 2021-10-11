eafb7fbe975016c140020f10a644203c0c700f6e
Luiz Felipe Montuani <luiz.silva16@estudante.ufla.br>
2021-10-11 10:27:05 -0300

    tentativa de estruturacao dos dados

--- argumento1.py
+++ argumento1.py
@@ -1,3 +1,4 @@
+from os import stat
 import sys
 
 runScript = sys.argv[1:]
@@ -9,7 +10,28 @@ except:
 text = file.read().splitlines()
 
 transitions = []
+transitions2 = []
+transitions3 = []
 input = text[-1]
+initialState = 1
+aux = 0
+white = 3
 
 for line in text[1:len(text)-2]:
     transitions.append(line)
+    transitions2.append(line)
+    transitions3.append(line)
+
+for item in input:
+    if item == '1':
+        aux += 1
+    else:
+        # verificar no transitions (aux e estado atual)
+        # c, initialState = funcao()
+        aux = 0
+
+# funcao retorna a letra e o proximo estado
+
+
+def funcao():
+    return a, b
