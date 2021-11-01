44e155c73e38fae95b1f9d75bb03da352327d8ba
Luiz Felipe Montuani <luiz.silva16@estudante.ufla.br>
2021-10-31 23:21:16 -0300

    with one tape

--- argumento1.py
+++ argumento1.py
@@ -12,26 +12,54 @@ text = file.read().splitlines()
 transitions = []
 transitions2 = []
 transitions3 = []
-input = text[-1]
-initialState = 1
-aux = 0
-white = 3
+input = text[-1].split('0')
+auxWhile = 0
+state = '1'
+aux = ''
+letra = ''
+direction = ''
+
 
 for line in text[1:len(text)-2]:
-    transitions.append(line)
+    transitions.append(line.split('0'))
     transitions2.append(line)
     transitions3.append(line)
 
-for item in input:
-    if item == '1':
-        aux += 1
+
+# funcao retorna a nova letra, o proximo estado e a direcao
+
+
+def transiction(state, letra):
+    isCorrect = False
+    newState = ''
+    newWord = ''
+    direction = ''
+
+    for line in transitions:
+        if (not isCorrect):
+            if(line[0] == state and line[1] == letra):
+                isCorrect = True
+                newState = line[2]
+                newWord = line[3]
+                direction = line[4]
+    return newState, newWord, direction
+
+
+while(auxWhile < len(input)):
+    if input[auxWhile] != '':
+        aux = input[auxWhile]
+        state, letra, direction = transiction(state, aux)
+        input[auxWhile] = letra
+
+        if direction == '1':
+            auxWhile += 1
+        else:
+            auxWhile -= 1
     else:
-        # verificar no transitions (aux e estado atual)
-        # c, initialState = funcao()
-        aux = 0
+        auxWhile += 1
 
-# funcao retorna a letra e o proximo estado
-
-
-def funcao():
-    return a, b
+for item in input:
+    if item != '':
+        print(item, end='0')
+print(end='00')
+print()
